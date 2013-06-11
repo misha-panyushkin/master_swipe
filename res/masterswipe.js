36c8b70d68659e0dc76538dd385f3cc2b552d0d6
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-06-11 20:05:08 +0400

    fixing elements blinking on transition end. - not fixing delay between touchstart and swipe begin.

--- res/masterswipe.js
+++ res/masterswipe.js
@@ -27,7 +27,8 @@ var swipe = function () {
         y = y || 0;
         z = z || 0;
         speed = speed || 0.0001;
-        this.target.style.webkitTransition = "all " + speed + "s";
+        this.target.style.webkitTransitionProperty  = "-webkit-transform";
+        this.target.style.webkitTransitionDuration  = speed + "s";
         this.target.style.webkitTransform = "translate3d(" + x + "px, " + y + "px, " + z + "px)";
     }
 
@@ -35,10 +36,17 @@ var swipe = function () {
     SWIPE.prototype.setCallback = function (f) {
         this.callback = function (swiped) {
             return function () {
-                swiped.target.style.webkitTransition = "none";
-                swiped.target.style.webkitTransform = "translate3d(" + 0 + "px, " + 0 + "px, " + 0 + "px)";
+
+                swiped.target.style.webkitTransition = "";
+                swiped.target.style.webkitTransform  = "";
+
                 swiped.target.style.left  = swiped.start_rectangle.left + swiped.XShift + "px";
                 swiped.target.style.top   = swiped.start_rectangle.top  + swiped.YShift + "px";
+
+                //swiped.target.style.webkitTransitionProperty  = "-webkit-transform";
+                //swiped.target.style.webkitTransitionDuration  = 0 + "ms";
+                //swiped.target.style.webkitTransform = "translate3d(" + 0 + "px, " + 0 + "px, " + 0 + "px)";
+
                 f();
             }
         } (this)
@@ -60,7 +68,7 @@ var swipe = function () {
 
     SWIPE.prototype.steer = function (x, y, z, speed, easing) {
         var args = Array.prototype.splice.call(arguments, 0);
-        args[0] = this.ontheway_rectangle.left    - this.start_rectangle.left + (args[0] || 0);
+        args[0] = this.ontheway_rectangle.left   - this.start_rectangle.left + (args[0] || 0);
         args[1] = this.ontheway_rectangle.top    - this.start_rectangle.top  + (args[1] || 0);
         args[3] = args[3] || 0;
         sliding.apply(this, args);
@@ -71,13 +79,17 @@ var swipe = function () {
         this.YShift = y;
         this.ZShift = z;
         var args = Array.prototype.splice.call(arguments, 0);
-        args[3] = !isNaN( args[3] ) && args[3].toString() || 1.4;
+        args[3] = !isNaN( args[3] ) && args[3].toString() || 0.2;
         addSwipeEndListener.call(this);
         sliding.apply(this, args);
     };
 
     SWIPE.prototype.rollback = function () {
-        sliding.call(this, 0, 0, 0, 2);
+        /*this.XShift = 0;
+        this.YShift = 0;
+        this.ZShift = 0;
+        addSwipeEndListener.call(this);*/
+        sliding.call(this, 0, 0, 0, .4);
     };
 
     return function (target) {
