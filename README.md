6dd6019a76a5ce1399daca52e77782411a71d8a4
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-08-01 15:16:45 +0400

    master swipe updates. - simplifying class structure; - adding suitable readme.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ master_swipe
 ======
 
 Master swipe class. Based on making and operating with swipes.
+
+___
+
+Swipe class is simple as its could be. 
+
+There is a simple function in window called: 
+
+`swipe`
+
+Pass a document element you want to animate as first argument into it:
+
+`swipe (element)`
+
+It'will return an object with several methods:
+
+	setCallback: function ( function_Shoul_Call_On_Animation_Finished ) {},
+	steer:		 fucntion ( x, y, z, speed, easing ) {},
+	to:			 fucntion ( x, y, z, speed, easing ) {}
--- res/masterswipe.js
+++ res/masterswipe.js
@@ -1,6 +1,15 @@
-var swipe = function () {
-    // Private variable;
-    var targets = [];
+/*
+ * The Swipe Methods Collection.
+ *
+ * Created by Misha Panyushkin.
+ * misha.panyushkin@gmail.com
+ *
+ * 01.08.2013
+ * */
+
+! function (window, undefined) {
+
+    var grasped = [];
 
     // Private methods.
     function addSwipeEndListener () {
@@ -13,37 +22,44 @@ var swipe = function () {
     }
 
     function sliding (x, y, z, speed, easing) {
+
         x = x || 0;
         y = y || 0;
         z = z || 0;
-        speed = speed || 0.0001;
+
         this.target.style.webkitTransitionProperty  = "-webkit-transform";
         this.target.style.webkitTransitionDuration  = speed + "s";
         this.target.style.webkitTransform = "translate3d(" + x + "px, " + y + "px, " + z + "px)";
+
     }
 
-
-    var SWIPE = function (target) {
-        this.target = target;
-        this.start_rectangle    = {};
-        this.ontheway_rectangle = {
-            left:   0,
-            top:    0
-        };
-        this.callback = null;
+    var swipe = function (node) {
+        return new swipe.like.magic(node);
     };
 
     // Public methods.
-    SWIPE.like = SWIPE.prototype = {
+    swipe.like = {
+
+        constructor: swipe,
+
+        magic: function (target) {
+            this.target = target;
+            this.setCallback(function () {});
+        },
+
         setCallback: function (f) {
             this.callback = function (swiped) {
                 return function () {
 
+                    removeSwipeEndListener.call(swiped);
+
+                    var rect = swiped.target.getBoundingClientRect();
+
                     swiped.target.style.webkitTransition = "";
                     swiped.target.style.webkitTransform  = "";
 
-                    swiped.target.style.left  = swiped.start_rectangle.left + swiped.XShift + "px";
-                    swiped.target.style.top   = swiped.start_rectangle.top  + swiped.YShift + "px";
+                    swiped.target.style.left  = rect.left + swiped.XShift + "px";
+                    swiped.target.style.top   = rect.top  + swiped.YShift + "px";
 
                     //swiped.target.style.webkitTransitionProperty  = "-webkit-transform";
                     //swiped.target.style.webkitTransitionDuration  = 0 + "ms";
@@ -53,41 +69,30 @@ var swipe = function () {
                 }
             } (this)
         },
-        grasp: function () {
-            this.start_rectangle = this.ontheway_rectangle = this.target.getBoundingClientRect()
-        },
-        freeze: function () {
-            removeSwipeEndListener.call(this);
-            this.ontheway_rectangle = this.target.getBoundingClientRect();
-            sliding.call(this,
-                this.ontheway_rectangle.left    - this.start_rectangle.left,
-                this.ontheway_rectangle.top     - this.start_rectangle.top,
-                0,
-                0);
-        },
+
         steer: function (x, y, z, speed, easing) {
             var args = Array.prototype.splice.call(arguments, 0);
-            args[0] = this.ontheway_rectangle.left   - this.start_rectangle.left + (args[0] || 0);
-            args[1] = this.ontheway_rectangle.top    - this.start_rectangle.top  + (args[1] || 0);
             args[3] = args[3] || 0;
             sliding.apply(this, args);
         },
+
         to: function (x, y, z, speed, easing) {
+
             this.XShift = x;
             this.YShift = y;
             this.ZShift = z;
+
             var args = Array.prototype.splice.call(arguments, 0);
             args[3] = !isNaN( args[3] ) && args[3].toString() || 0.2;
+
             addSwipeEndListener.call(this);
+
             sliding.apply(this, args);
-        },
-        rollback: function () {
-            sliding.call(this, 0, 0, 0, .4);
         }
     };
 
-    return function (target) {
-        targets[target.id] = targets[target.id] || new SWIPE(target);
-        return targets[target.id];
-    };
-}();
+    swipe.like.magic.prototype = swipe.like;
+
+    window.swipe = swipe;
+
+} (window);
